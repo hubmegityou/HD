5f45d5b28efcb05f36308a2cec6c1974b27d968b
hubmegityou <hubmegityou@users.noreply.github.com>
2017-07-10 13:58:05 +0200

    dbinfo update

--- dbinfo.php
+++ dbinfo.php
@@ -6,5 +6,31 @@
 		$db_users_pass = 'password';
 		$db_users_fname = 'first_name';
 		$db_users_lname = 'last_name';
-		$db_users_emmail = 'email';
+		$db_users_email = 'email';
+		$db_users_function = 'user_function';
+	
+	$db_task_tab = 'task';
+		$db_task_id = 'tast_ID';
+		$db_task_name = 'name';
+		$db_task_description = 'description';
+		$db_task_sdate = 'start_date';
+		$db_task_edate = 'end_date';
+		$db_task_userid = 'user_ID';
+		
+	$db_subtast_tab = 'subtask';	
+		$db_subtast_id = 'subtask_ID';
+		$db_subtast_taskid = 'task_ID';
+		$db_subtast_name = 'name';
+		$db_subtast_sdate = 'start_date';
+		$db_subtast_edate = 'end_date';
+		$db_subtast_userid = 'user_ID';
+		
+	$db_functions_tab = 'functions';
+		$db_functions_id = 'function_ID';
+		$db_functions_desc = 'function_description';
+		
+	$db_attachment_tab = 'attachment';
+		$db_attachment_id = 'att_ID';
+		$db_attachment_name = 'attachment';
+		$db_attachment_taskid = 'task_ID';
 ?>

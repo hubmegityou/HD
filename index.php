c90796b11a7c3498ed4c56dac9a68be587c89493
hubmegityou <hubmegityou@users.noreply.github.com>
2017-07-10 11:52:46 +0200

    poprawki znowu, zmiana nazwy

--- index.php
+++ index.php
@@ -15,12 +15,12 @@
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<title>Osadnicy - gra przeglądarkowa</title>
+	<title>Help Desk - projekt</title>
 </head>
 
 <body>
 	
-	<form action="logowanie.php" method="post">
+	<form action="login.php" method="post">
 	
 		Login: <br /> <input type="text" name="login" /> <br />
 		Hasło: <br /> <input type="password" name="pass" /> <br /><br />

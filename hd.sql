73ba22bde7b1a7bdf8448c7e5afc763e3a4e112c
hubmegityou <ola.stolorz@gmail.com>
2017-07-17 10:53:24 +0200

    bazaaa

--- hd.sql
+++ hd.sql
@@ -3,15 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
-<<<<<<< HEAD:hd.sql
--- Czas generowania: 17 Lip 2017, 08:18
+-- Czas generowania: 17 Lip 2017, 10:45
 -- Wersja serwera: 10.1.24-MariaDB
 -- Wersja PHP: 7.1.6
-=======
--- Czas generowania: 11 Lip 2017, 15:02
--- Wersja serwera: 10.1.22-MariaDB
--- Wersja PHP: 7.1.4
->>>>>>> origin/master:projekt.sql
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -24,14 +18,12 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Baza danych: `hd`
---
-
 -- --------------------------------------------------------
 
 --
 -- Struktura tabeli dla tabeli `attachment`
+--
+-- Data utworzenia: 11 Lip 2017, 11:44
 --
 
 CREATE TABLE `attachment` (
@@ -40,16 +32,28 @@ CREATE TABLE `attachment` (
   `task_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- RELATIONSHIPS FOR TABLE `attachment`:
+--   `task_ID`
+--       `task` -> `task_ID`
+--
+
 -- --------------------------------------------------------
 
 --
 -- Struktura tabeli dla tabeli `functions`
+--
+-- Data utworzenia: 11 Lip 2017, 11:44
 --
 
 CREATE TABLE `functions` (
   `function_ID` int(11) NOT NULL,
   `function_description` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONSHIPS FOR TABLE `functions`:
+--
 
 --
 -- Zrzut danych tabeli `functions`
@@ -67,6 +71,8 @@ INSERT INTO `functions` (`function_ID`, `function_description`) VALUES
 --
 -- Struktura tabeli dla tabeli `subtask`
 --
+-- Data utworzenia: 14 Lip 2017, 12:40
+--
 
 CREATE TABLE `subtask` (
   `subtask_ID` int(11) NOT NULL,
@@ -79,11 +85,19 @@ CREATE TABLE `subtask` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- RELATIONSHIPS FOR TABLE `subtask`:
+--   `task_ID`
+--       `task` -> `task_ID`
+--   `user_ID`
+--       `users` -> `user_ID`
+--
+
+--
 -- Zrzut danych tabeli `subtask`
 --
 
 INSERT INTO `subtask` (`subtask_ID`, `task_ID`, `name`, `start_date`, `end_date`, `description`, `user_ID`) VALUES
-(1, 1, 'huehuehuehue', '2017-07-03', '2017-07-04', 'hdhdhdhdhdhdhdhdhdh jsjsjsjsj kkdekkeke ksjsjsssjsj kkekekwkwkwk kssdnxmxkxk xnxsjsjsjs AAALŁŁŁŁąąąąąććć', 3),
+(1, 1, 'huehuehuehue', '2017-07-03', '2017-07-04', 'hdhdhdhdhdhdhdhdhdh jsjsjsjsj kkdekkeke ksjsjsssjsj kkekekwkwkwk kssdnxmxkxk xnxsjsjsjs AAAL�?�?�?�?ąąąąąććć', 3),
 (2, 1, 'efghjkl', '2017-07-19', '2017-07-28', 'wefghjkjhnbvdswertyujhnbvfdew45tryhgfdwertghfdertgfdertgfdertgfdertf frerfderdffdew egngtrergre yt434rgr43rtg erght54tgtr54trg wrett4regbr ewrgtr4rtgbrttg ewrfgbgfre4rgbr4 wedfgbfregr wedfbgregr43 efdbgregfr43 edffgrgbfr4 wdfbfrefgr43 wsdvcfregbfre4efre4 wdfbgtrfvfgr4 wdfbgre4fre34 fdfgrefbfgr43 wedfgtr4 ', 4),
 (3, 1, 'efghjkl', '2017-07-19', '2017-07-28', 'wefghjkjhnbvdswertyujhnbvfdew45tryhgfdwertghfdertgfdertgfdertgfdertf frerfderdffdew egngtrergre yt434rgr43rtg erght54tgtr54trg wrett4regbr ewrgtr4rtgbrttg ewrfgbgfre4rgbr4 wedfgbfregr wedfbgregr43 efdbgregfr43 edffgrgbfr4 wdfbfrefgr43 wsdvcfregbfre4efre4 wdfbgtrfvfgr4 wdfbgre4fre34 fdfgrefbfgr43 wedfgtr4 ', 4);
 
@@ -91,6 +105,8 @@ INSERT INTO `subtask` (`subtask_ID`, `task_ID`, `name`, `start_date`, `end_date`
 
 --
 -- Struktura tabeli dla tabeli `task`
+--
+-- Data utworzenia: 11 Lip 2017, 11:44
 --
 
 CREATE TABLE `task` (
@@ -101,6 +117,12 @@ CREATE TABLE `task` (
   `end_date` date NOT NULL,
   `user_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- RELATIONSHIPS FOR TABLE `task`:
+--   `user_ID`
+--       `users` -> `user_ID`
+--
 
 --
 -- Zrzut danych tabeli `task`
@@ -114,6 +136,8 @@ INSERT INTO `task` (`task_ID`, `name`, `description`, `start_date`, `end_date`, 
 --
 -- Struktura tabeli dla tabeli `users`
 --
+-- Data utworzenia: 11 Lip 2017, 11:44
+--
 
 CREATE TABLE `users` (
   `user_ID` int(11) NOT NULL,
@@ -126,20 +150,20 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- RELATIONSHIPS FOR TABLE `users`:
+--   `user_function`
+--       `functions` -> `function_ID`
+--
+
+--
 -- Zrzut danych tabeli `users`
 --
 
 INSERT INTO `users` (`user_ID`, `first_name`, `last_name`, `email`, `login`, `password`, `user_function`) VALUES
-<<<<<<< HEAD:hd.sql
 (1, 'hgfds', 'hgfd', 'hgew', 'gf', 'ggrerwq', 1),
 (3, 'imię', 'nazwisko', 'email', 'login', 'hasło', 4),
 (4, 'kjhkj', 'hoopk', 'mni', 'lklk', 'lkklm', 5),
-(5, 'asdas', 'asdasd', 'asdasd', 'aaa', 'qqq', 3);
-=======
-(3, 'imię', 'nazwisko', 'email', 'login', 'df0090a4c59f141d203ef0dbd5710fc0', 4),
-(5, 'administrator', 'asdasd', 'asdasd', 'aaa', 'b2ca678b4c936f905fb82f2733f5297f', 1),
-(6, 'Jan', 'Kowalski', 'grafik@firma.pl', 'graf', '62f2596b743b732c244ca5451a334b4f', 3);
->>>>>>> origin/master:projekt.sql
+(5, 'asdas', 'asdasd', 'asdasd', 'aaa', 'qqq', 1);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -208,7 +232,7 @@ ALTER TABLE `task`
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Ograniczenia dla zrzutów tabel
 --

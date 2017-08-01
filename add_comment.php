cc2db21a536e5a32ed67960f87095287aaf8c161
hubmegityou <hubmegityou@users.noreply.github.com>
2017-08-01 18:21:01 +0200

    notyfikacje subtaski

--- add_comment.php
+++ add_comment.php
@@ -1,4 +1,3 @@
-
 <?php
 
     session_start();
@@ -18,12 +17,14 @@
         $task_id= $_POST['myTID'];
         $sql = "INSERT INTO $db_messages_tab ($db_messages_id, $db_messages_userid, $db_messages_taskid, $db_messages_date, $db_messages_text) VALUES (NULL,'". $_SESSION['id']."', '$task_id', '$curr_timestamp', '$comment')";
         $connection->query($sql);
+        /*
         $text = "Dodano komentarz do aktywnego zadania";
         $sql = "INSERT INTO $db_notifications_tab ($db_notifications_id, $db_notifications_date, $db_notifications_taskid, $db_notifications_subtaskid, $db_notifications_text) VALUES (NULL, '".date('Y-m-d H:i:s')."', '$task_id', '0', '$text')";
-        echo $sql;;
+        echo $sql;
         if ($result = $connection -> query($sql))
-                echo "dziala";
+                //info kontrolne, że działa
+         */
     }    
     $connection->close();
-    //header("Location: tasks_all.php?sid=$subtask_id&tid=$task_id");
+    header("Location: tasks_all.php?sid=$subtask_id&tid=$task_id");
 ?>

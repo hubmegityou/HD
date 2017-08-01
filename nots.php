76df21d3f57a779d69a9a46f820d171727603862
srututututu <ola.stolorz@vp.pl>
2017-08-01 13:27:20 +0200

    notifications

--- nots.php
+++ nots.php
@@ -99,10 +99,10 @@
                 require_once "objects.php";
                 $connection = db_connection();
        
-                           $sql= "select $db_notifications_tab.$db_notifications_text ,$db_notifications_tab.$db_notifications_date, $db_nots_user_tab.$db_nots_user_id  from $db_notifications_tab left join $db_nots_user_tab ON $db_notifications_tab.$db_notifications_id = $db_nots_user_tab.$db_nots_user_notiicationid  WHERE $db_nots_user_tab.$db_nots_user_userid=".$_SESSION['id'];
+                           $sql= "select $db_notifications_tab.$db_notifications_subtaskid ,$db_notifications_tab.$db_notifications_taskid, $db_notifications_tab.$db_notifications_text ,$db_notifications_tab.$db_notifications_date, $db_nots_user_tab.$db_nots_user_id  from $db_notifications_tab left join $db_nots_user_tab ON $db_notifications_tab.$db_notifications_id = $db_nots_user_tab.$db_nots_user_notiicationid  WHERE $db_nots_user_tab.$db_nots_user_userid=".$_SESSION['id'];
                            $result = $connection->query($sql);
                            while($row = $result->fetch_assoc()){
-                           echo "<a href='' id='$row[$db_nots_user_id] onclick='delete_nots($row[$db_nots_user_id])' ><img src='template/assets/img/trash.png' /></a>"."[".$row[$db_notifications_date]."]    ".$row[$db_notifications_text].'<br><br>';}           
+                           echo "<a href='tasks_all.php?sid=$row[$db_notifications_subtaskid]&tid=$row[$db_notifications_taskid]'  style='color:black; text-decoration: none'>$row[$db_notifications_date]     $row[$db_notifications_text]</a>".'<br><br>';}           
                    
                            ?>
                
@@ -122,20 +122,3 @@
     ?>
 </script>
 
-
-
-<script>
- 
- function delete_nots(id){
- // tu jakoś trzeba przesłać to dalej do tego poniżej xD 
-   };
-</script>
- 
-<?php  
- require_once "database/dbinfo.php";
- require_once "objects.php";
- $connection = db_connection();
- $sql = "DELETE FROM $db_nots_user_tab WHERE $db_nots_user_id=$id";
- $connection->query($sql);
-
-?>

f91d25747ca135b0fd0f93c78d3fef2aa5797f7c
hubmegityou <hubmegityou@users.noreply.github.com>
2017-08-02 17:40:20 +0200

    drobna optymalizacja SQL

--- add_comment.php
+++ add_comment.php
@@ -18,11 +18,16 @@
         $sql = "INSERT INTO $db_messages_tab ($db_messages_id, $db_messages_userid, $db_messages_taskid, $db_messages_date, $db_messages_text) VALUES (NULL,'". $_SESSION['id']."', '$task_id', '$curr_timestamp', '$comment')";
         $connection->query($sql);
         /*
-        $text = "Dodano komentarz do aktywnego zadania";
-        $sql = "INSERT INTO $db_notifications_tab ($db_notifications_id, $db_notifications_date, $db_notifications_taskid, $db_notifications_subtaskid, $db_notifications_text) VALUES (NULL, '".date('Y-m-d H:i:s')."', '$task_id', '0', '$text')";
-        echo $sql;
-        if ($result = $connection -> query($sql))
+            $text = "Dodano komentarz do aktywnego zadania";
+            $sql = "INSERT INTO $db_notifications_tab ($db_notifications_id, $db_notifications_date, $db_notifications_taskid, $db_notifications_subtaskid, $db_notifications_text) VALUES (NULL, '".date('Y-m-d H:i:s')."', '$task_id', '0', '$text')";
+            echo $sql;
+            if ($result = $connection -> query($sql)){
                 //info kontrolne, że działa
+                $notificationid = $connection->insert_id;
+                
+                // wpis do tabeli nots_user (???)    
+         
+           }
          */
     }    
     $connection->close();
--- addsubt.php
+++ addsubt.php
@@ -4,7 +4,7 @@
  *  - ŁADOWANIE DANYCH DO BAZY (done)
  *  - KOMUNIKATY O BŁĘDZIE
  *  - POPRAWNE PRZEKIEROWANIA PO NIEWYPEŁNIENIU FORMULARZA (done)
- *  - WERYFIKACJA DATY (CZY KONIEC NIE WCZEŚNIEJ NIŻ ZADANIE GŁÓWNE LUB WYŚWIETLANIE KOŃCOWEJ DATY)
+ *  - WERYFIKACJA DATY (CZY KONIEC NIE WCZEŚNIEJ NIŻ ZADANIE GŁÓWNE LUB WYŚWIETLANIE KOŃCOWEJ DATY) (done)
  */
     session_start();
     if (($_POST['user'] == '') || ($_POST['task'] == '') || (!isset($_POST['topic'])) || (!isset($_POST['description'])) || (!isset($_POST['etime']))){
@@ -21,8 +21,7 @@
         $edate = $_POST['etime'];
         
         if ($sdate > $edate){
-            
-                
+                            
             echo "<script type=\"text/javascript\">alert('Niepoprawna data');</script>"; // sa ale tak jakby ich nie było bo strona przeskakuje dalej i aletu nie widac xDDDD
             header('Location: add_subtasks.php');
             close();
@@ -36,27 +35,17 @@
         $sql = "INSERT INTO $db_subtask_tab ($db_subtask_id,$db_subtask_taskid,$db_subtask_name,$db_subtask_sdate,$db_subtask_edate,$db_subtask_description,$db_subtask_userid) VALUES (NULL,$taskid,'$topic','$sdate','$edate','$desc',$userid)";
         if ($result = $connection->query($sql)){
             echo "<script type=\"text/javascript\">alert('dodawanie zakończone');</script>";// sa ale tak jakby ich nie było bo strona przeskakuje dalej i aletu nie widac xDDDD
-            
-            $sql = "SELECT $db_subtask_id FROM $db_subtask_tab WHERE $db_subtask_taskid='$taskid' AND $db_subtask_name='$topic' AND $db_subtask_userid='$userid'";
-            $result = $connection->query($sql);
-            $row = $result -> fetch_assoc();
-            $subtaskid = $row[$db_subtask_id];
-            
+            $subtaskid = $connection->insert_id;
             $text = "Masz przydzielone nowe zadanie";
             $curr_timestamp = date('Y-m-d H:i:s');
             $sql = "INSERT INTO $db_notifications_tab ($db_notifications_id, $db_notifications_date, $db_notifications_taskid, $db_notifications_subtaskid, $db_notifications_text) VALUES (NULL, '".$curr_timestamp."', '$taskid', '$subtaskid', '$text')";        }
-            if ($result = $connection -> query($sql));
+            if ($result = $connection -> query($sql));{
                     //info testowe: działa
-            
-            $sql = "SELECT $db_notifications_id FROM $db_notifications_tab WHERE $db_notifications_date='$curr_timestamp' AND $db_notifications_subtaskid='$subtaskid'";
-            $result = $connection->query($sql);
-            $row = $result -> fetch_assoc();
-            $notificationid = $row[$db_notifications_id];
-            
-            $sql = "INSERT INTO $db_nots_user_tab ($db_nots_user_id, $db_nots_user_notificationid, $db_nots_user_userid, $db_nots_user_readnots) VALUES (NULL, '$notificationid', '$userid', '0')";
-            if ($result = $connection->query($sql));
-                    //info testowe: działa
-            
+                $notificationid = $connection->insert_id;       
+                $sql = "INSERT INTO $db_nots_user_tab ($db_nots_user_id, $db_nots_user_notificationid, $db_nots_user_userid, $db_nots_user_readnots) VALUES (NULL, '$notificationid', '$userid', '0')";
+                if ($result = $connection->query($sql));
+                        //info testowe: działa
+            }
     }       
     $connection->close();
     header('Location: main.php');

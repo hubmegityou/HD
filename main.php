2699d119b4dd316d48acd488f661d1647307d356
hubmegityou <hubmegityou@users.noreply.github.com>
2017-08-06 18:38:52 +0200

    ,jhkk

--- main.php
+++ main.php
@@ -53,16 +53,16 @@
                                 $row[$db_subtask_edate] = date("Y-m-d", $row[$db_subtask_edate]);
                                 $rows= "{ title: '".$row[$db_subtask_name]."', start: '".$row[$db_subtask_sdate]."', end: '".$row[$db_subtask_edate]."'},";    
                                 echo $rows;}  
+                      
+                            while($row = $result->fetch_assoc()){
+                                $row[$db_task_edate] = strtotime("$row[$db_task_edate] + 1 day");
+                                $row[$db_task_edate] = date("Y-m-d", $row[$db_task_edate]);
+                                $rows= "{ title: '".$row[$db_task_name]."', start: '".$row[$db_task_sdate]."', end: '".$row[$db_task_edate]."'},";    
+                                echo $rows;
+                                }
+                            $connection->close();
                         }
                         //else info o błędzie
-
-                        while($row = $result->fetch_assoc()){
-                        $row[$db_task_edate] = strtotime("$row[$db_task_edate] + 1 day");
-                        $row[$db_task_edate] = date("Y-m-d", $row[$db_task_edate]);
-                        $rows= "{ title: '".$row[$db_task_name]."', start: '".$row[$db_task_sdate]."', end: '".$row[$db_task_edate]."'},";    
-                        echo $rows;
-                         }
-                         $connection->close();
 ?>  
 			]
 		});

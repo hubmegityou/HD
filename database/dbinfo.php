70512c2b544d59caa8c19f520a3d895b9e3ea365
hubmegityou <hubmegityou@users.noreply.github.com>
2017-08-16 13:57:30 +0200

    final

--- database/dbinfo.php
+++ database/dbinfo.php
@@ -58,8 +58,8 @@
                 $db_nots_user_id = 'nots_ID';
                 $db_nots_user_notificationid = 'notification_ID';
                 $db_nots_user_userid = 'user_ID';
-                $db_nots_user_taskid = '';
-                $db_nots_user_subtaskid = '';
+                $db_nots_user_taskid = 'task_ID';
+                $db_nots_user_subtaskid = 'subtask_ID';
                 $db_nots_user_readnots = 'read_nots';
                 $db_nots_user_delete= 'delete_n';
 ?>

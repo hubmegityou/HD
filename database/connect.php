f48dceb62467441d4e6d526b7dfa64a617ca7920
hubmegityou <hubmegityou@users.noreply.github.com>
2017-08-17 07:41:06 +0200

    ddd

--- database/connect.php
+++ database/connect.php
@@ -1,8 +1,12 @@
 <?php
 
-	$host = "mysql3.mydevil.net";
-	$db_user = "m1147_hdvena";
-	$db_pass = "ji0ywlLExhlPOZ6aRy3N";
-	$db_name = "m1147_hdvena";
+//	$host = "mysql3.mydevil.net";
+//	$db_user = "m1147_hdvena";
+//	$db_pass = "ji0ywlLExhlPOZ6aRy3N";
+//	$db_name = "m1147_hdvena";
 
+        $host = "localhost";
+	$db_user = "root";
+	$db_pass = "";
+	$db_name = "hd";
 ?>
